6d9dd168a755de877e47c4795d49c383ec4d43df
Peter Blanchfield <zygomy@gmail.com>
2022-01-27 11:50:42 +0000

    GymData Test shows error
    the BMI did not get set when we added the mass and height

---
+++ UseGymData/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GymLibrary;
+
+namespace UseGymData
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GymData gymData = new GymData(1);
+            gymData.HeightCm = 165;
+            gymData.MassKg = 60;
+            Console.WriteLine("Gym member has BMI " +  gymData.BodyMassIndex);
+        }
+    }
+}
